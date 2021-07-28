cf6a782fdb016a432078f627de4490c9b2253295
Sunil Shahi <sunil.shahi@fusionstak.com>
2021-07-28 18:28:49 -0500

    Add project files.

---
+++ TestAsposeImage2/Pages/Privacy.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Aspose.Words;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace TestAsposeImage2.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+        public ActionResult OnGet()
+        {
+            var document = new Document("testfile.docx");
+            var ms = new MemoryStream();
+            document.Save(ms, SaveFormat.Pdf);
+            return File(ms.ToArray(), "application/octet-stream", "testfile.pdf");
+        }
+    }
+}
